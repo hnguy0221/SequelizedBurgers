968550ab456ba7e6c6e46653bdb7d7f33503d81c
hnguy0221 <hnguy0221@gmail.com>
2017-09-02 16:18:22 -0600

    Added README.md and api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -31,9 +31,10 @@ module.exports = function(app) {
         burger_name = burger_name.substring(2, burger_name.length);
         var eatenStr = burger_name + " (Eaten by " + customer_name + ")";
         console.log("eatenStr: '" + eatenStr + ";");
-        if (customer_name === "")
-        {
-            return;
+
+        //if the customer's name is empty or blank, the return.
+        if (customer_name === "") {
+            res.redirect("/");
         }
         //check to see if customer's name exists. If exists, then update
         // Burgers table with that customer's id.
@@ -42,8 +43,7 @@ module.exports = function(app) {
                 cust_nm: customer_name
             }
         }).then(function(cust_data) {
-            if (cust_data !== null && cust_data !== undefined)
-            {
+            if (cust_data !== null && cust_data !== undefined) {
                 // Update takes in two arguments, an object describing the properties we want to update,
                 // and another "where" object describing the burgers we want to update
                 db.Burger.update({
@@ -63,8 +63,7 @@ module.exports = function(app) {
             }
             //if the customer does not exist, create it and then update the Burgers table with 
             //the newly created customer's id.
-            else
-            {
+            else {
                 db.Customer.create({
                     cust_nm: customer_name
                 }).then(function(result) {
@@ -91,18 +90,23 @@ module.exports = function(app) {
 
     // POST route for saving a new burger
     app.post("/burgers/create", function(req, res) {
+
+        //if the burger's name is empty or blank, refresh the page.
         var burger_nm = req.body.burger_name.trim();
         console.log("burger_name: '" + burger_nm + "'");
-        if (burger_nm === "")
-        {
-            return;
+        if (burger_nm === "") {
+            res.redirect("/");
         }
-        db.Customer.findOne({order: [['cust_nm', 'ASC']]}).then(function(cust_data) {
+        db.Customer.findOne({
+            where: {
+                cust_nm: "UNKNOWN"
+            }
+            //order: [['id', 'ASC']]
+        }).then(function(cust_data) {
             // We have access to the customer as an argument inside of the callback function
             //res.json(cust_data);
             console.log(cust_data); 
-            if (cust_data === null || cust_data === undefined)
-            { 
+            if (cust_data === null || cust_data === undefined) { 
                 db.Customer.create({
                     cust_nm: "UNKNOWN"
                 }).then(function(result) {
@@ -119,9 +123,8 @@ module.exports = function(app) {
                         res.redirect("/");
                     });
                 });
-            }
-            else
-            {
+            } 
+            else {
                 db.Burger.create({
                     burger_name: burger_nm,
                     CustomerId: cust_data.id
